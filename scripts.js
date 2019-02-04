282e2eb47f804d527da4e57d0a57758b69d4ad26
Manav Misra <manavm@visionwebsoft.com>
2019-02-04 13:37:17 -0600

    Ask user for input and store in variable.

--- scripts.js
+++ scripts.js
@@ -1,1 +1,5 @@
-
+/**
+ * 1. Ask the user how high they want to sum up numbers until.
+ * 2. Store the user response in a variable.
+ */
+const stopValue = prompt("How high do you want to sum numbers up until?");
