37aa97d8a33972a1866eb980adb58c9b57299cb1
manavm1990 <manavm@visionwebsoft.com>
2019-02-07 08:16:33 -0600

    Change from parseInt to using Number consttructor.

--- scripts.js
+++ scripts.js
@@ -1,7 +1,8 @@
 const stopValue = prompt("How high do you want to sum numbers up until?");
 let startValue = prompt("Where do you want to start the summing from?")
 
-startValue = Number.parseInt(startValue, 10);
+/**
+startValue = Number(startValue);
 
 let sum = 0;
 
