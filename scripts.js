09ff348d5429350cf763f4a7b9aa05b2786a272a
manavm1990 <manavm@visionwebsoft.com>
2019-02-06 18:26:58 -0600

    Parse user input for starting number.

--- scripts.js
+++ scripts.js
@@ -1,1 +1,4 @@
 const stopValue = prompt("How high do you want to sum numbers up until?");
+let startValue = prompt("Where do you want to start the summing from?")
+
+startValue = Number.parseInt(startValue, 10);
