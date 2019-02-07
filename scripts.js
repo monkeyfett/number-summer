c1092164b144f0a239ad64ecaaf270e0d2562083
manavm1990 <manavm@visionwebsoft.com>
2019-02-06 18:27:23 -0600

    Initialize sum and start 'loop' counting.

--- scripts.js
+++ scripts.js
@@ -2,3 +2,12 @@ const stopValue = prompt("How high do you want to sum numbers up until?");
 let startValue = prompt("Where do you want to start the summing from?")
 
 startValue = Number.parseInt(startValue, 10);
+
+let sum = 0;
+
+while (startValue <= stopValue) {
+  sum += startValue;
+  startValue++;
+}
+
+console.log(sum);
