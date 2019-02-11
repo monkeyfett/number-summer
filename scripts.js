fe47c9b0ca3872a1797d7b1b56236fa6ee776080
Manav Misra <manavm@visionwebsoft.com>
2019-02-11 13:42:04 -0600

    Add psuedo-code

--- scripts.js
+++ scripts.js
@@ -1,3 +1,10 @@
+/**
+ *  1. Capture user input from #start.
+ *  2. "" "" from #stop.
+ *  3. Listen for a submit and start adding.
+ *  4. Update <output> to show the total.
+ */
+
 const stopValue = prompt("How high do you want to sum numbers up until?");
 
 let startValue = prompt("Where do you want to start the summing from?");
