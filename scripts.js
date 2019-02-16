61748fd129d02309493ef889b6b1f8f83ea1ea94
manavm1990 <manavm@visionwebsoft.com>
2019-02-16 17:21:14 -0600

    Add comments

--- scripts.js
+++ scripts.js
@@ -3,6 +3,11 @@
  *  4. Update <output> to show the total.
  */
 
+/**
+ * Query the document with a CSS selector.
+ * We would expect to get back an HTML element.
+ * Next, we access the 'value' property of this element.
+ */
 const stopValue = document.querySelector("#stop").value;
 console.log(stopValue);
 
@@ -10,9 +15,10 @@ console.log(stopValue);
 const startValue = document.querySelector("#start").value;
 console.log(startValue);
 
+// Listen for a 'submit' event on the 'form.'
 document.querySelector("form").addEventListener(
   "submit",
-  // Add a callback fxn.
+  // Add a CALLBACK fxn.
   () => {
     console.log("submitted");
   }
